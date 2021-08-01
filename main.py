34cb8529483dfccb6a08c274338be348ef2b580d
auddy <soumyadeepauddy1999@gmail.com>
2021-08-01 12:17:25 +0530

    flask part added

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask,render_template,request,Markup,jsonify
+import warnings
+warnings.filterwarnings("ignore")
+
+
+# FLASK PART
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+	return render_template('app.html')
+
+@app.route('/show',methods=['POST'])
+def show():
+	print(str(request.form["b64"]))
+	return jsonify({'verdict':"1"})
+
+
+if(__name__ == '__main__'):
+	app.run(debug = True)
+
+
+
--- static/script.js
+++ static/script.js
@@ -1,4 +1,3 @@
-
 var width = 320;    // We will scale the photo width to this
 var height = 0;     // This will be computed based on the input stream
 
@@ -8,6 +7,8 @@ var video = null;
 var canvas = null;
 var photo = null;
 var startbutton = null;
+var b64Text = null;
+var caption = null;
 
 $(document).ready(function(){
   console.log("hello");
@@ -19,6 +20,8 @@ function initVideo(){
   canvas = document.getElementById('canvas');
   photo = document.getElementById('photo');
   startbutton = document.getElementById('startbutton');
+  b64Text = document.getElementById('b64Text');
+  caption = document.getElementById('caption');
 
   navigator.mediaDevices.getUserMedia({ video: true, audio: false })
   .then(function(stream) {
@@ -66,12 +69,34 @@ function takepicture() {
     context.drawImage(video, 0, 0, width, height);
 
     var data = canvas.toDataURL('image/png');
+    console.log(data);
     photo.setAttribute('src', data);
+    b64Text.setAttribute('value', data);
   } else {
     clearphoto();
   }
 }
 
+
+
+
+
+//Ajax
+
+$("#mainForm").on('submit', function(event){
+  $.ajax({
+    url: "/show",
+    type: "post",
+    data: {
+      // These are params sent to show route
+      b64: $("#b64Text").val()
+    }
+  })
+  .done(function(data){
+    console.log(data.verdict);
+  })
+  event.preventDefault();
+});
 
 
 
