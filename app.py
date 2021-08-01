6f002909dd9864aff80a868f8cc0467d8149e1e3
auddy <soumyadeepauddy1999@gmail.com>
2021-08-01 12:43:55 +0530

    flask part added

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-	return render_template('app.html')
+	return render_template('index.html')
 
 # @app.route('/show',methods=['POST'])
 # def show():
